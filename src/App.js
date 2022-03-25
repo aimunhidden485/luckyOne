6457f33c88b86ec4eac0a28cd11038447953aae7
aimunhidden485 <aimunnaharr@gmail.com>
2022-03-25 21:11:16 +0600

    data added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  const products =[
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 1},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 2},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 3},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 4},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 5},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 6},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 7},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 8},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 9},
+    {name: 'Aveeno mosturizing lotion', price: 2050, image: 'https://carnesia.com/wp-content/uploads/2021/11/unnamed-3-264x347.jpg',id: 10}
+  ]
   return (
-   
+   <div>
+     <div className="header">
+       <h3>Skincare Station</h3>
+     </div>
+     
+   </div>
   );
 }
 
